ea12b0536ae15fe7a2ecd5e36b1f3c88c8cb1e06
Keith C. Ivey <keith@iveys.org>
2010-06-04 10:50:10 -0400

    update plugin version in demo

--- jquery.jwplayer.js
+++ jquery.jwplayer.js
@@ -6,7 +6,7 @@ Copyright 2010, Smokescreen Corporation
 Dual licensed under the MIT and GPL licenses
 http://www.opensource.org/licenses/mit-license.php
 http://www.gnu.org/licenses/gpl.html
-Version 0.110 (2010-05-24)
+Version 0.111 (2010-06-03)
 */
 
 var pluginName = 'jwPlayer';
@@ -197,6 +197,7 @@ $.fn[pluginName] = function (opts) {
             if (extraPlayerready) {
                 extraPlayerready(obj);
             }
+            $this.addClass(pluginName); // to signal it's finished
         };
         opts.flashvars.playerready = fnName;
         $this.empty().flash( {
