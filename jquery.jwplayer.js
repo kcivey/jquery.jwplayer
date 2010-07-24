5750334e9a54cf8bf25ed601d199c6053520ee9c
Keith C. Ivey <keith@iveys.org>
2010-07-24 18:18:35 -0400

    add empty() back in, since jquery.swfobject has problems

--- jquery.jwplayer.js
+++ jquery.jwplayer.js
@@ -6,7 +6,7 @@ Copyright 2010, Smokescreen Corporation
 Dual licensed under the MIT and GPL licenses
 http://www.opensource.org/licenses/mit-license.php
 http://www.gnu.org/licenses/gpl.html
-Version 0.113 (2010-07-23)
+Version 0.114 (2010-07-24)
 */
 
 var pluginName = 'jwPlayer';
@@ -200,7 +200,7 @@ $.fn[pluginName] = function (opts) {
             }
         };
         opts.flashvars.playerready = fnName;
-        $this.flash( {
+        $this.empty().flash( {
             swf: opts.swf,
             id: id,
             name: id,
